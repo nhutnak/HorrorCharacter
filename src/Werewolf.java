c921954c84a8f9fe2986ab27b0e9606a36033f78
Nate <nhutnak5964@westfield.ma.edu>
2025-09-24 11:16:32 -0400

    wolf class

--- src/Werewolf.java
+++ src/Werewolf.java
@@ -1,25 +1,47 @@
-public class Werewolf extends HorrorCharacter
+public class Werewolf extends HorrorCharacter implements Transformable
 {
-    public Werewolf(String name, int health, Vulnerability[] vulnerabilities)
+    private Boolean transformed;
+    public Werewolf(String name, int health, Vulnerability[] vulnerabilities, Boolean transformed)
     {
         super(name, health, vulnerabilities = new Vulnerability[]{Vulnerability.SUNLIGHT, Vulnerability.HOLY_WATER});
+        this.transformed = transformed;
     }
 
     @Override
     void attack()
     {
-        System.out.println("The Werewolf (in human form) slaps you");
+        if(!transformed)
+        {
+            System.out.println("The Werewolf (in human form) slaps you");
+        }
+        else
+        {
+            System.out.println("The Werewolf (in wolf form) slashes you");
+        }
     }
 
     @Override
     void flee()
     {
-        System.out.println("The Werewolf (in human form) turns around and runs away");
+        if(!transformed)
+        {
+            System.out.println("The Werewolf (in human form) turns around and runs away");
+        }
+        else
+        {
+            System.out.println("The Werewolf (in wolf form) runs away on all fours");
+        }
     }
 
     @Override
     Vulnerability[] getVulnerabilities()
     {
         return super.getVulnerabilities();
+    }
+
+    @Override
+    public void transform()
+    {
+        transformed = !transformed;
     }
 }
