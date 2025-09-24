dd84b2272d8123916494a3d5d87f5d849fba186d
Nate <nhutnak5964@westfield.ma.edu>
2025-09-24 11:13:45 -0400

    vamp class

--- src/Vampire.java
+++ src/Vampire.java
@@ -1,19 +1,34 @@
-public class Vampire extends HorrorCharacter
+public class Vampire extends HorrorCharacter implements Transformable
 {
-    public Vampire(String name, int health, Vulnerability[] vulnerabilities)
+    private Boolean transformed;
+
+
+    public Vampire(String name, int health, Vulnerability[] vulnerabilities, Boolean transformed)
     {
         super(name, health, vulnerabilities = new Vulnerability[]{Vulnerability.SUNLIGHT, Vulnerability.SILVER});
+        this.transformed = transformed;
     }
 
     @Override
     void attack()
     {
-        System.out.println("The vampire (in human form) bites you.");
+        if(!transformed)
+        {
+            System.out.println("The vampire (in human form) bites you.");
+        }
+        else
+        {
+            System.out.println("The vampire (in bat form) swoops down and scratches you.");
+        }
     }
 
     @Override
     void flee()
     {
+        if(!transformed)
+        {
+            System.out.println("The vampire (in bat form) flies away.");
+        }
         System.out.println("The vampire (in human form) dissapeared.");
     }
 
@@ -23,4 +38,9 @@ public class Vampire extends HorrorCharacter
         return super.getVulnerabilities();
     }
 
+    @Override
+    public void transform()
+    {
+        transformed = !transformed;
+    }
 }
